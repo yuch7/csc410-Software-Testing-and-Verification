2bd7c6ab550abe8611d78e5f97cbac41a3ecea8d
yuch7 <yuchen7990@gmail.com>
2016-02-23 17:06:44 -0500

    music in my chairs

--- a1/musicalchairs.py
+++ a1/musicalchairs.py
@@ -13,6 +13,11 @@ _in = open(sys.argv[1], "r")
 # Helper variable definitions, Z3 variables + functions #
 ######################################################### 
 
+n = int(_in.readline())
+S = int(_in.readline())
+sub_set = map(int, _in.readline().strip().strip('[').strip(']').split(','))
+board = [Int("%s" % i) for i in range(n)]
+
 ##################  Your Code Here  #####################
 
 #########################################################
@@ -21,12 +26,27 @@ _in = open(sys.argv[1], "r")
 
 ##################  Your Code Here  #####################
 
+must_move = []
+for i in range(n):
+	if i in sub_set:
+		if ((board[i] + S) >= n):
+			must_move.append((board[i] >= 0))
+			must_move.append((board[i] < i))
+	for j in range(n):
+		if i != j:
+			must_move.append((board[i] != board[j]))
+
+distinct = [Distinct(board)]
+
+in_seats = [(i < n) for i in board]
+not_neg = [(i >= 0) for i in board]
+
 #########################################################
 #         Call the solver and print the answer          #
 #########################################################
 
 # The final formula going in. Change this to your actual formula
-F = simplify(Bool('p')==Bool('p'))
+F = must_move + distinct + in_seats + not_neg
 
 # a Z3 solver instance
 solver = Solver()
@@ -39,7 +59,7 @@ if isSAT == sat:
    m = solver.model()
 
    ##############  Complete the Output  #################
-   print 
+   print "%s" % m
 else:
    print "no solution possible"
 
