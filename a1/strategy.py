3a6037a45767d0b1acadd14180ede32165091cfa
yuch7 <yuchen7990@gmail.com>
2016-02-26 13:27:19 -0500

    strategy done

--- a1/strategy.py
+++ a1/strategy.py
@@ -20,18 +20,51 @@ for i in range(3):
 
 ##################  Your Code Here  #####################
 
+aWin = Bool("aWin")
+aHigh = Bool("aHigh")
+aMed = Bool("aMed")
+aLow = Bool("aLow")
+
+bWin = Bool("bWin")
+bHigh = Bool("bHigh")
+bMed = Bool("bMed")
+bLow = Bool("bLow")
+
+board = []
+for i in range(3):
+	board.append([])
+	for j in range(3):
+		board[i].append(Bool("%s%s" % (i, j)))
+
 #########################################################
 #        The actual constraints for the problem         #
 #########################################################
 
 ##################  Your Code Here  #####################
 
+boardc = []
+for i in range(3):
+  for j in range(3):
+    boardc.append(board[i][j] == (r[i][j][0] < r[i][j][1]))
+wins = []
+wins.append(aWin == (aHigh or aMed or aLow))
+wins.append(bWin == (bHigh or bMed or bLow))
+
+eq = []
+eq.append(aHigh == reduce(lambda x,y: And(x,y), board[0], True))
+eq.append(aMed  == reduce(lambda x,y: And(x,y), board[1], True))
+eq.append(aLow  == reduce(lambda x,y: And(x,y), board[2], True))
+eq.append(bHigh == reduce(lambda x,y: And(x,y), [board[i][0] for i in range(3)], True))
+eq.append(bMed  == reduce(lambda x,y: And(x,y), [board[i][1] for i in range(3)], True))
+eq.append(bLow  == reduce(lambda x,y: And(x,y), [board[i][2] for i in range(3)], True))
+
+
 #########################################################
 #         Call the solver and print the answer          #
 #########################################################
 
 # The final formula going in. Change this to your actual formula
-F = simplify(Bool('p')==Bool('p'))
+F = eq + wins + boardc
 
 # a Z3 solver instance
 solver = Solver()
@@ -42,11 +75,17 @@ isSAT = solver.check()
 # print the result
 if isSAT == sat:
    # evaluate the final array from the model, and re-map the guest id's to the result
-   m = solver.model()   
+  m = solver.model()   
    
    ##############  Complete the Output  #################
-   print "Player A:"
-   print "Player B:"
+  pa = "No dominant strategy"
+  pb = "No dominant strategy"
+  if (is_true(m.eval(aWin))):
+    pa = "The dominant strategy is %s" % ('High' if is_true(m.eval(aHigh)) else ('Med' if is_true(m.eval(aMed)) else 'Low'))
+  if (is_true(m.eval(bWin))):
+    pb = "The dominant strategy is %s" % ('High' if is_true(m.eval(bHigh)) else ('Med' if is_true(m.eval(bMed)) else 'Low'))
+  print "Player A: %s" % pa 
+  print "Player B: %s" % pb
 else:
-   print "no solution possible"
+  print "no solution possible"
 
